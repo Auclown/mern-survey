db3ef491201a2dcd3fe24a226ef0af8c17de69d7
Auclown <auclown@gmail.com>
2020-12-04 23:43:19 +1300

    4th/Dec/2020 - Setting up Google Oauth...

--- src/app.js
+++ src/app.js
@@ -3,7 +3,27 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 
 const app = express();
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
 module.exports = app;
