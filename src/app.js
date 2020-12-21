ee5147ab1e090cfceb54b60ee24c4aa22a6a7229
Auclown <auclown@gmail.com>
2020-12-22 00:23:44 +1300

    22nd/Dec/2020 - Stripe library to create charge request using Stripe API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 
 require("./models/User");
@@ -8,6 +9,7 @@ require("./db/connectDB")();
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -18,5 +20,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 module.exports = app;
